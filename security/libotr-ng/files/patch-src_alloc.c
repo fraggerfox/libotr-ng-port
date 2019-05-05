f4cc8e10760ef8e7e9e7d1256183138355d2f586
fraggerfox <santhosh.raju@gmail.com>
2019-05-05 14:27:00 -0500

    Documented the patches. Removed MD specific patching.
    Identified the issues were not MD specific, but was an issue in the code base.
    Modified and documented the patches to reflect the changes.

--- security/libotr-ng/files/patch-src_alloc.c
+++ security/libotr-ng/files/patch-src_alloc.c
@@ -1,3 +1,7 @@
+"%zu" should be used for size_t variables and not "%lu".
+
+XXX: Discuss this with upstream people.
+
 --- src/alloc.c.orig	2019-05-05 15:22:09 UTC
 +++ src/alloc.c
 @@ -39,7 +39,7 @@ INTERNAL /*@only@*/ /*@notnull@*/ void *otrng_xmalloc(
@@ -5,7 +9,7 @@
        oom_handler();
      }
 -    fprintf(stderr, "fatal: memory exhausted (xmalloc of %lu bytes).\n", size);
-+    fprintf(stderr, "fatal: memory exhausted (xmalloc of %u bytes).\n", size);
++    fprintf(stderr, "fatal: memory exhausted (xmalloc of %zu bytes).\n", size);
      exit(EXIT_FAILURE);
    }
  
@@ -14,7 +18,7 @@
        oom_handler();
      }
 -    fprintf(stderr, "fatal: memory exhausted (xrealloc of %lu bytes).\n", size);
-+    fprintf(stderr, "fatal: memory exhausted (xrealloc of %u bytes).\n", size);
++    fprintf(stderr, "fatal: memory exhausted (xrealloc of %zu bytes).\n", size);
      exit(EXIT_FAILURE);
    }
  
--- security/libotr-ng/files/patch-src_client.c
+++ security/libotr-ng/files/patch-src_client.c
@@ -1,3 +1,10 @@
+time_t is a Machine-Dependent variable and doing comaprisons against
+uint32_t is not wise, for example the comparison check raises a warning
+in i386 where size(time_t) is 4 (in contrast to 8 in amd64). This may
+lead to undesirable consequences.
+
+XXX: Discuss this with the upstream people.
+
 --- src/client.c.orig	2019-05-05 15:21:28 UTC
 +++ src/client.c
 @@ -563,7 +563,7 @@ INTERNAL void otrng_client_expire_sessions(otrng_clien
