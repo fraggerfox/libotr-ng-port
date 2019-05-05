55d71e36f296cce4f0d88798d91a40bf7a2792a9
fraggerfox <santhosh.raju@gmail.com>
2019-05-05 15:28:28 -0500

    Fixed a typo in patch documentation, size => sizeof.

--- security/libotr-ng/files/patch-src_client.c
+++ security/libotr-ng/files/patch-src_client.c
@@ -1,6 +1,6 @@
 time_t is a Machine-Dependent variable and doing comaprisons against
 uint32_t is not wise, for example the comparison check raises a warning
-in i386 where size(time_t) is 4 (in contrast to 8 in amd64). This may
+in i386 where sizeof(time_t) is 4 (in contrast to 8 in amd64). This may
 lead to undesirable consequences.
 
 XXX: Discuss this with the upstream people.
