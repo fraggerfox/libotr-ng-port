aa0cc5147d9a8530d1e695473e73d7e5695af3fe
fraggerfox <santhosh.raju@gmail.com>
2019-05-21 10:02:47 -0500

    Re-worded the patch comment to be more meaningful.

--- security/libotr-ng/files/patch-src_client.c
+++ security/libotr-ng/files/patch-src_client.c
@@ -1,7 +1,8 @@
-time_t is a Machine-Dependent variable and doing comaprisons against
-uint32_t is not wise, for example the comparison check raises a warning
-in i386 where sizeof(time_t) is 4 (in contrast to 8 in amd64). This may
-lead to undesirable consequences.
+time_t is a Machine-Dependent variable (signed int) and doing
+comaprisons against uint32_t is not wise, for example the comparison
+check raises a warning in FreeBSD/i386 (where sizeof(time_t) is 4)
+with Wsign-compare flag during compile. This may lead to undesirable
+consequences.
 
 XXX: Discuss this with the upstream people.
 
